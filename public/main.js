07e22635e63094465cbfd9f733f28cf1d67becbe
Justin Swiger <justin.swiger@me.com>
2019-11-12 15:48:44 -0500

    finished explorer mode

--- public/main.js
+++ public/main.js
@@ -1,7 +1,19 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const qs = e => document.querySelector(e)
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?'
+let apiKey = 'APPID=281ebdcd59ed51cf8ad5380190437020'
+
+const getWeather = async () => {
+  const zipCode = qs('input.zipInput').value
+  const resp = await fetch(`${url}zip=${zipCode}&${apiKey}&units=imperial`)
+  const weatherData = await resp.json()
+  displayData(weatherData)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = weatherData => {
+  qs('.temp').textContent = 'Temperature: ' + weatherData.main.temp + '°'
+  qs('.conditions').textContent =
+    'Conditions: ' + weatherData.weather[0].description
+}
+
+qs('.getWeather').addEventListener('click', getWeather)
